87d373a7e6be3be45b0c14e26334a2509debf4df
John Lane <john.lane93@me.com>
2021-01-15 16:36:17 -0600

    Add some items to list

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonSearchbar, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonSearchbar, IonTitle, IonToolbar, IonList, IonItem } from '@ionic/react';
 
 const Search: React.FC = () => {
   return (
@@ -13,11 +13,28 @@ const Search: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Search</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonList>
+          <IonItem>Amsterdam</IonItem>
+          <IonItem>Bogota</IonItem>
+          <IonItem>Buenos Aires</IonItem>
+          <IonItem>Cairo</IonItem>
+          <IonItem>Dhaka</IonItem>
+          <IonItem>Edinburgh</IonItem>
+          <IonItem>Geneva</IonItem>
+          <IonItem>Genoa</IonItem>
+          <IonItem>Glasgow</IonItem>
+          <IonItem>Hanoi</IonItem>
+          <IonItem>Hong Kong</IonItem>
+          <IonItem>Islamabad</IonItem>
+          <IonItem>Istanbul</IonItem>
+          <IonItem>Jakarta</IonItem>
+          <IonItem>Kiel</IonItem>
+          <IonItem>Kyoto</IonItem>
+          <IonItem>Le Havre</IonItem>
+          <IonItem>Lebanon</IonItem>
+          <IonItem>Lhasa</IonItem>
+          <IonItem>Lima</IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
