c4f97d6aba4599739bc34bed27382285e5aeeeb2
John Lane <john.lane93@me.com>
2022-06-16 13:51:39 -0500

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
-  extends: ['react-app'],
+  extends: ['react-app', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   rules: {
-    'react/jsx-filename-extension': 'off',
-    //You can override any rules you want
-  },
+    'react/jsx-filename-extension': 'off' //You can override any rules you want
+
+  }
 };
